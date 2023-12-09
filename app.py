062690d8beca6155178ff155e2f8b13f6b51bdf9
Veronica Ni <vni1294@gmail.com>
2023-12-08 21:34:53 -0500

    added bootstrap skeleton, flash messages

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ toolbar = DebugToolbarExtension(app)
 
 print("app.config.url: ", app.config['SQLALCHEMY_DATABASE_URI'])
 
+
 @app.get('/')
 def show_pets():
     """Homepage displays all pets."""
@@ -31,7 +32,6 @@ def show_pets():
     print("pets: ", pets)
 
     return render_template('homepage.html', pets=pets)
-
 
 
 @app.route("/add", methods=["GET", "POST"])
@@ -48,47 +48,44 @@ def add_pet():
         notes = form.notes.data
 
         new_pet = Pet(name=name,
-                    species=species,
-                    photo_url=photo_url,
-                    age=age,
-                    notes=notes)
+                      species=species,
+                      photo_url=photo_url,
+                      age=age,
+                      notes=notes)
 
         db.session.add(new_pet)
         db.session.commit()
 
-        #TODO:flash message
-
-        flash(f"Added {name}")
+        flash(f"Added {name}!")
         return redirect("/")
 
     else:
         return render_template(
             "pet_add_form.html", form=form)
-    
+
+
 @app.route("/<int:pet_id>", methods=["GET", "POST"])
 def show_pet_details(pet_id):
     """Show details about the pet. 
         Allows the user to edit the pet information.
      """
-    
+
     pet = Pet.query.get_or_404(pet_id)
 
     form = EditPetForm(obj=pet)
-    
+
     if form.validate_on_submit():
         # no destructuring possible?
-        pet.photo_url = form.photo_url.data
+        pet.photo_url = (
+            form.photo_url.data if form.photo_url.data else pet.photo_url)
         pet.notes = form.notes.data
         pet.available = form.available.data
 
         db.session.add(pet)
         db.session.commit()
 
-        #How to keep the notes field prepopulated on form submit success?
-
-        #TODO:flash message
-        flash(f"Edited {pet.name}")
+        flash(f"Updated {pet.name}!")
         return redirect(f"/{pet_id}")
-    
+
     else:
         return render_template("pet_details.html", pet=pet, form=form)
